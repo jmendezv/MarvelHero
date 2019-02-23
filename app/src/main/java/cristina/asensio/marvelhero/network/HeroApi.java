5185ee90471f793909d9fe884ed804bf0396ab3c
casensio <cristina.asensio.munoz@gmail.com>
2019-02-23 16:20:20 +0100

    Implemented Hero service and api

---
+++ app/src/main/java/cristina/asensio/marvelhero/network/HeroApi.java
@@ -0,0 +1,45 @@
+package cristina.asensio.marvelhero.network;
+
+
+import com.serjltt.moshi.adapters.Wrapped;
+import com.squareup.moshi.Moshi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class HeroApi {
+
+    private static final String BASE_URL = "http://gateway.marvel.com/v1/public/";
+
+    private static Retrofit retrofit;
+    private static HeroService repoService;
+
+    public static HeroService getInstance() {
+
+        if (repoService != null) {
+            return repoService;
+        }
+
+        if (retrofit == null) {
+            initializeRetrofit();
+        }
+
+        repoService = retrofit.create(HeroService.class);
+        return repoService;
+    }
+
+    private static void initializeRetrofit() {
+        Moshi moshi = new Moshi.Builder()
+        .add(Wrapped.ADAPTER_FACTORY)
+        .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build();
+    }
+
+    private HeroApi() {
+
+    }
+}
---
+++ app/src/main/java/cristina/asensio/marvelhero/network/HeroService.java
@@ -0,0 +1,25 @@
+package cristina.asensio.marvelhero.network;
+
+import com.serjltt.moshi.adapters.Wrapped;
+
+import java.util.List;
+
+import cristina.asensio.marvelhero.model.Hero;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface HeroService {
+
+    String PARAM_NAME = "name=thor";
+    String PARAM_API_KEY = "apikey=8a42b42c15b9898a45b6da0bbe018757";
+    String PARAM_HASH = "hash=bc1f342fe6c53106d7acd60a7c8a3516";
+    String PARAM_TIMESTAMP = "ts=1";
+
+    @GET("/v1/public/characters?"
+            + PARAM_NAME + "&"
+            + PARAM_API_KEY + "&"
+            + PARAM_HASH + "&"
+            + PARAM_TIMESTAMP)
+    @Wrapped(path = {"data", "results"})
+    Call<List<Hero>> getCharacter();
+}
