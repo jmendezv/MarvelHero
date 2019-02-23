d7adfedb57dd889e02f4949e127afe0157a5ad0d
casensio <cristina.asensio.munoz@gmail.com>
2019-02-23 16:23:52 +0100

    Created the models for hero's data

---
+++ app/src/main/java/cristina/asensio/marvelhero/model/Link.java
@@ -0,0 +1,28 @@
+package cristina.asensio.marvelhero.model;
+
+public class Link {
+
+    private final String type;
+    private final String url;
+
+    public Link(String type, String url) {
+        this.type = type;
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" +
+                "type='" + type + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
